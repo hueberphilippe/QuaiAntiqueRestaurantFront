a37d08a313d8db33ca1cd82ffc4411aca76c68da
hueberphilippe <hueberphilippe@outlook.fr>
2025-05-01 21:43:58 +0200

    Add galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
     /*new Route("/Galerie", "Galerie", "/pages/gallerie.html", "/js/galerie.js"),*/
 ];
     
