ef580c94574acd26d1fc39e3e1860cb35a297e64
hueberphilippe <hueberphilippe@outlook.fr>
2025-05-01 23:14:49 +0200

    Add signin signout password

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
-    new Route("/signin", "Galerie", "/pages/signin.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/account.html"),
+    new Route("/editPassword", "Mot de passe", "/pages/editPassword.html"),
     /*new Route("/Galerie", "Galerie", "/pages/gallerie.html", "/js/galerie.js"),*/
 ];
     
