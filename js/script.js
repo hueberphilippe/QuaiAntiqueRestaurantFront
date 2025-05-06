aaef97f84e1bc01d99c6461bc5ee5b5e1a5cacec
hueberphilippe <hueberphilippe@outlook.fr>
2025-05-06 12:01:05 +0200

    Add getInfosUser par cde fetch

--- js/script.js
+++ js/script.js
@@ -112,3 +112,33 @@ function sanitizeHtml(text) {
     tempHtml.textContent = text;
     return tempHtml.innerHTML;
 }
+
+//Fonction pour récupérer les informations utilisateur avec fetch
+function getInfosUser() {
+    let myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", getToken());
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch(apiUrl + "account/me", requestOptions)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            else {
+                console.log("Impossible de récupérer les informations utilisateur");
+            }
+        })
+
+        .then(result => {
+            //console.log(result);
+            return result;
+        })
+
+        .catch(error => {
+            console.error("erreur lors de la récupération des données utilisateur", error);
+        });
+}
